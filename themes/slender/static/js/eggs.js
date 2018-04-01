f3a156675a709ee1e05db14b9ab794fb03543283
Maartje Eyskens <meyskens@me.com>
2018-04-01 22:19:53 +0100

    Add easter egg

---
+++ themes/slender/static/js/eggs.js
@@ -0,0 +1,16 @@
+const now = new Date();
+
+const loadEgg = () => {
+    clippy.load('Clippy', (agent) => {
+        agent.show();
+        setInterval(agent.speak, 5000, "So liking this site, huh?")
+        setInterval(agent.speak, 10000, "You know, I only show once a year?")
+        setInterval(agent.speak, 20000, "Want to sign my petition to show up at halloween too??")
+        setInterval(agent.animate, 30000)
+    });
+}
+
+if (now.getDate() == 1 && now.getMonth() == 3) {
+    loadEgg();
+}
+
